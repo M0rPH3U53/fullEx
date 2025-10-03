eb8df356f00ad3113a155743a54eb84d2c114d45
M0rPH3U53 <144029241+M0rPH3U53@users.noreply.github.com>
2025-10-03 11:20:10 +0200

    Update fullEx.sh

--- fullEx.sh
+++ fullEx.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [[ "$1" == "-help" ]]; then
-    echo "Commande dispo:"
+if [[ "$*" == "-help" ]]; then
+    echo "Commande :"
     echo "  -PwnKit64/32   : Exploite PwnKit en architecture 64/32"
     echo "  -DirtyCow      : Exploite DirtyCow"
     echo "  -DirtyPipe     : Exploite DirtyPipe"
     echo "  -Chwoot        : Exploite Sudo Chroot"
     echo "  -SudoHost      : Exploite Sudo Hostname "
     echo "  -Baron         : Exploite sudo_Baron"
-    ehco "  -check         : Verifie si gcc est installer"
+    echo "  -check         : Verifie si gcc est installer"
     exit 0
 fi
