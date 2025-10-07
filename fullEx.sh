f932049ae3cf6b010b0f71ca279d3dd72d4a373c
M0rPH3U53 <144029241+M0rPH3U53@users.noreply.github.com>
2025-10-07 10:05:47 +0200

    Update fullEx.sh

--- fullEx.sh
+++ fullEx.sh
@@ -4,11 +4,11 @@ arch="64 32"
 
 if [[ "$*" == "-help" ]]; then
     echo "Commande :"
-    echo "  -PwnKit64/32   : Exploite PwnKit en architecture 64/32"
-    echo "  -DirtyCow      : Exploite DirtyCow"
-    echo "  -DirtyPipe     : Exploite DirtyPipe"
-    echo "  -Chroot        : Exploite Sudo Chroot"
-    echo "  -SudoHost      : Exploite Sudo Hostname "
+    echo "  -DirtyCow      : Exploite DirtyCow - CVE-2016-5195"
+    echo "  -PwnKit64/32   : Exploite PwnKit en architecture 64/32 - CVE-2021-4034"
+    echo "  -DirtyPipe     : Exploite DirtyPipe - CVE-2022-0847"
+    echo "  -Chroot        : Exploite Sudo Chroot - CVE-2025-32463"
+    echo "  -SudoHost      : Exploite Sudo Hostname - CVE-2025-32462 "
     echo "  -check         : Verifie si gcc est installer"
     exit 0
 fi
