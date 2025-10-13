91e36fdaf21a86e31a554d77f6ea16f0be1d57ff
M0rPH3U53 <144029241+M0rPH3U53@users.noreply.github.com>
2025-10-13 14:26:36 +0000

    Update fullEx.sh

--- fullEx.sh
+++ fullEx.sh
@@ -6,24 +6,31 @@ if [[ "$*" == "-help" ]]; then
     echo "cmd:"
     echo "   -LinPeas       Execute LinPeas"
     echo "   -LaZagne       Execute LaZagne"
+    echo "   -Overlays      Overlays64 - CVE-2015-1328"
     echo "   -DirtyCow      DirtyCow - CVE-2016-5195"
     echo "   -PwnKit64/32   PwnKit64/32 - CVE-2021-4034"
     echo "   -DirtyPipe     DirtyPipe - CVE-2022-0847"
     echo "   -Chroot        Sudo Chroot - CVE-2025-32463"
     echo "   -SudoHost      Sudo Hostname - CVE-2025-32462 "
     echo "   -check         Verifie si gcc & python est installer"
-    echo "   -perm          Application des droit sur le dossier fullEx"
+    echo "   -perm          Applique les droit d'execution"
+    echo "   -compile       Compile le binaire"
     exit 0
 fi
 
-# Application des droit sur le dossier fullEx
+# Application des droit d'execution
 if [[ "$*" == "-perm" ]]; then
    find ${PWD} -type f -exec chmod +x {} \;
 fi
 
+# if [[ "$*" == "-compile" ]]; then
+#    gcc ${file} -o ${bin}
+#    echo "[+] Compilation reussi !"
+# fi
+
 # LinPeas
 if [[ "$*" == "-LinPeas" ]]; then
-   "${PWD}/linpeas.sh"
+   "${PWD}linpeas/linpeas.sh"
 fi
 
 # LaZagne
@@ -33,10 +40,5 @@ fi
 
 # PwnKit
 if [[ "$*" == "-PwnKit${arch}" ]]; then
-   "${PWD}/PwnKit/PwnKit${arch}"
-fi
-
-# Sudo-Host
-if [[ "$*" == "-SudoHost" ]]; then
-     "${PWD}/Sudo-host/exploit"
+   "${PWD}exploits/PwnKit/PwnKit${arch}"
 fi
